f4ec918a7898a52a39b82c995efa54f55d79db5a
yexuman <yexuman@qianxin.com>
2019-12-26 10:47:39 +0800

    Java8之接口的default的关键字

--- src/main/java/com/yexuman/tool/java8/functionalinter/Test.java
+++ src/main/java/com/yexuman/tool/java8/functionalinter/Test.java
@@ -8,5 +8,21 @@ public class Test {
     public static void main(String[] args) {
         FunctionalInterDemo demo = (name, age) -> System.out.println("我叫" + name + "，我今年" + age + "岁了");
         demo.say("金庸", 99);
+
+
+        //Lambda的写法 ABC
+        System.out.println(toUpperString(str -> str.toUpperCase(), "abc"));
+        //匿名内部类的写法
+        System.out.println(toUpperString(new MyNumber<String>() {
+            @Override
+            public String getValue(String s) {
+                return s;
+            }
+        },"abc"));
+
+    }
+
+    public static String toUpperString(MyNumber<String> mn, String str) {
+        return mn.getValue(str);
     }
 }
